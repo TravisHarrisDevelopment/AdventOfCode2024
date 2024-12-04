d06e1aa43d5f9b57521ecaf187f63a16e5839c3a
Travis Harris <travis.harris.development@gmail.com>
2024-12-04 06:51:54 -0500

    initial work on day4

--- 2024AdventOfCode.Tests/Day4Tests.cs
+++ 2024AdventOfCode.Tests/Day4Tests.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace _2024AdventOfCode.Tests
 {
     public class Day4Tests
     {
+        [Fact]
+        public void Day4ThrowsIfInputNotProvided()
+        {
+            //Arrange
+            string input = null;
+
+            //Act
+            var exception = Assert.Throws<ArgumentNullException>( () => new Day4(input));
+
+            //Assert
+            Assert.Equal("input", exception.ParamName);
+        }
+
+        [Theory]
+        [InlineData ("Day4TestInput", "The word XMAS appears 18 times.")]
+        public void Day4RunFirstReturnsCorrectWordCount(string input, string expected)
+        {
+            //Arrange
+            var sut = new Day4(input);
+
+            //Act
+            var actual = sut.RunFirst();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- 2024AdventOfCode/Day4.cs
+++ 2024AdventOfCode/Day4.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2024AdventOfCode.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,32 @@ namespace _2024AdventOfCode
 {
     public class Day4 : IDay
     {
+        private readonly string _input;
+
+        public string[] WordSearch { get; set; }
+
+        public Day4(string input)
+        {
+            if (input == null || input=="")
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            _input = input;
+            InputHelper.GetInput(input);
+        }
+
+
         public string RunFirst()
         {
-            throw new NotImplementedException();
+            //word can be horizontal, vertical, diagonal, written backwards
+            //check horizontal for XMAS and SAMX
+            int horizontalCount = CheckHorizontal("XMAS");
+            int backwardsCount = CheckHorizontal("SAMX");
+            //check vertical (up and down)
+            int verticalCountDown = CheckVertical("XMAS");
+            int verticalCountUp = CheckVertical("SAMX");
+            //check diagonal (up , down for both 'XMAS' and 'SAMX'
         }
 
         public string RunSecond()
